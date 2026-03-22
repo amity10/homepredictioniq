c9edad8c523fce66e03eca94f0c300bca3210879
Amit Yadav <amityadav340360@gmail.com>
2026-03-22 19:34:45 +0530

    Fixed CORS and CSRF for Vercel frontend

--- backend/homeprice_backend/settings.py
+++ backend/homeprice_backend/settings.py
@@ -137,7 +137,16 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 
 
 CSRF_TRUSTED_ORIGINS = [
-   "https://homepredictioniq-ffn1.vercel.app"
+    "http://localhost:3000",
+    "http://127.0.0.1:8001",
+    "http://localhost:8001",
+    "http://127.0.0.1:8002",
+    "http://localhost:8002",
+    "http://127.0.0.1:8003",
+    "http://localhost:8003",
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+    "https://homepredictioniq-ffn1.vercel.app"
 ]
 
 CORS_ALLOWED_ORIGINS = [
